16fd7f7f5f9c525266836f90aba0a1af40ccc410
Tizery <reserved123@mail.ru>
2021-07-12 21:04:49 +0700

    hw6

---
+++ src/hw6/animals/Cat.java
@@ -0,0 +1,8 @@
+package hw6.animals;
+
+public class Cat extends Animal {
+
+    public Cat(String name, int maxDistToRun, int maxDistToSwim) {
+        super(name, maxDistToRun, maxDistToSwim);
+            }
+}

b295379583f0c55bc7e2921ae3e06eb1f107d66d
Tizery <reserved123@mail.ru>
2021-07-20 16:23:19 +0700

    hw8

---
+++ src/hw8/Obstacle.java
@@ -0,0 +1,6 @@
+package hw8;
+
+public interface Obstacle {
+
+    boolean action(JumpRunnable jr);
+}
---
+++ src/hw8/Robot.java
@@ -0,0 +1,44 @@
+package hw8;
+
+public class Robot implements JumpRunnable {
+
+    private String name;
+    private int maxRun;
+    private int maxJump;
+
+    public Robot(String name, int maxRun, int maxJump) {
+        this.name = name;
+        this.maxRun = maxRun;
+        this.maxJump = maxJump;
+    }
+
+    @Override
+    public boolean run(int dist) {
+        if (dist <= 0) {
+            System.out.println("Введено не еврное значение!");
+            return false;
+        }
+        if (dist <= maxRun) {
+            System.out.printf("%s успешно пробежал %d метров\n", name, dist);
+            return true;
+        } else {
+            System.out.printf("%s не смог пробежать %d метров\n", name, dist);
+            return false;
+        }
+    }
+
+    @Override
+    public boolean jump(int dist) {
+        if (dist <= 0) {
+            System.out.println("Введено не еврное значение!");
+            return false;
+        }
+        if (dist <= maxJump) {
+            System.out.printf("%s успешно прыгнул на %d метров\n", name, dist);
+            return true;
+        } else {
+            System.out.printf("%s не смог прыгнуть на %d метров\n", name, dist);
+            return false;
+        }
+    }
+}

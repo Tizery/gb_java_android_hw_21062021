9057e6e3c04bdfeeb114dcc3d2a283bda91c6f6d
Tizery <reserved123@mail.ru>
2021-07-22 19:05:24 +0700

    hw9

---
+++ src/hw9/MyArrayDataException.java
@@ -0,0 +1,21 @@
+package hw9;
+
+public class MyArrayDataException extends RuntimeException{
+
+    private int i;
+    private int j;
+
+    public MyArrayDataException(String message, int i, int j) {
+        super(message);
+        this.i = i;
+        this.j = j;
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public int getJ() {
+        return j;
+    }
+}
---
+++ src/hw9/MyArraySizeException.java
@@ -0,0 +1,8 @@
+package hw9;
+
+public class MyArraySizeException extends IndexOutOfBoundsException{
+
+    public MyArraySizeException(String s) {
+        super(s);
+    }
+}

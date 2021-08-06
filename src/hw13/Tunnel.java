2da4e7ca86be281d678bb81bed28675b54843e94
Tizery <reserved123@mail.ru>
2021-08-07 00:44:08 +0700

    hw13

---
+++ src/hw13/Tunnel.java
@@ -0,0 +1,33 @@
+package hw13;
+
+import java.util.concurrent.Semaphore;
+
+public class Tunnel extends Stage {
+
+    public Semaphore semaphore;
+
+    public Tunnel() {
+        semaphore = new Semaphore(Main.CARS_COUNT / 2);
+        this.length = 80;
+        this.description = "Тоннель " + length + " метров";
+    }
+
+    @Override
+    public void go(Car c) {
+        try {
+            if (!semaphore.tryAcquire()) {
+                System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+                semaphore.acquire();
+            }
+            System.out.println(c.getName() + " начал этап: " + description);
+            Thread.sleep(length / c.getSpeed() * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println(c.getName() + " закончил этап: " + description);
+            semaphore.release();
+        }
+      }
+}
+
+
